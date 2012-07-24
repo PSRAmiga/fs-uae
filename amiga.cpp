67c1f19783f5ec2ffaf085ecb1d532030b941a39
AlessandroMarioRiva <alessandromario.riva@gmail.com>
2012-07-24 17:04:39 +0200

    Allineamento grafica

--- amiga.cpp
+++ amiga.cpp
@@ -2,7 +2,7 @@
 #include "ui_amiga.h"
 #include <QFileDialog>
 #include <QColorDialog>
-#include <QPlastiqueStyle> /////funziona???
+#include <QPlastiqueStyle> //funziona??
 
 
 Amiga::Amiga(QWidget *parent) :
